847cd534f41ff5fc1d283803b7fefc0132f9619b
unknown <mdsabbirrahman2025@gmail.com>
2023-03-31 15:27:09 +0600

    first commit

---
+++ pages/admin/packages.js
@@ -0,0 +1,12 @@
+function Packages(){
+    return(
+        <>
+           <h3>Package 1</h3>
+           <h3>Package 2</h3>
+           <h3>Package 3</h3>
+
+        </>
+    )
+}
+
+export default Packages
